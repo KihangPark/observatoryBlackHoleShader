a30f004f7d8a617419c1ea9a3211065a7b90060e
KIHANG-PC\KIHANG <mathleona@gmail.com>
2018-02-04 01:29:44 -0800

    cleanup code

--- include/observatoryBlackHoleShader.h
+++ include/observatoryBlackHoleShader.h
@@ -14,5 +14,4 @@ miC_LINKAGE DLLEXPORT miBoolean observatoryBlackHoleShader(miColor*, miState*, s
 miC_LINKAGE DLLEXPORT miInteger observatoryBlackHoleShader_version();
 miC_LINKAGE DLLEXPORT void observatoryBlackHoleShader_exit(miState*, struct observatoryBlackHoleShader*);
 
-
 #endif
--- module/observatoryBlackHoleShader.cpp
+++ module/observatoryBlackHoleShader.cpp
@@ -1,7 +1,6 @@
-
 #include <iostream>
-#include "shader.h"
 #include <cmath>
+#include "shader.h"
 #include "observatoryBlackHoleShader.h"
 
 #define _USE_MATH_DEFINES
@@ -14,10 +13,8 @@ void observatoryBlackHoleShader_init(miState* state, struct observatoryBlackHole
 
 	if (!paras) {
         *inst_req = miTRUE;
-
 		cout << "---------------------------------------------------------" << endl;
         cout << "    observatoryBlackHoleShader, compiled on " << __DATE__ << endl;
-		cout << "    if you have any trouble about this shader, please send e-mail to mathleona@gmail.com" << endl;
         cout << "---------------------------------------------------------" << endl;
     }
 
@@ -28,17 +25,9 @@ miBoolean observatoryBlackHoleShader (
 {
 	miScalar transparency = *mi_eval_scalar(&params->transparency);
 	miScalar radius = *mi_eval_scalar(&params->radius);
-	//miBoolean debug = 1;
 	miBoolean debug = *mi_eval_boolean(&params->debug);
-	//cout << "debug : " << debug << endl;
-
-	if (debug == 1) {
-		cout << "ray type : " << state->type;
-		cout << "debug : " << debug << endl;
-	}
 
 	if ((state->type == 1)&&(state->parent->type == 0)) {
-	//if ((state->type == 1)&&(state->parent->type == 0)) {
 
 		miVector current = state->point;
 		miVector origin = state->org;
@@ -78,8 +67,6 @@ miBoolean observatoryBlackHoleShader (
 		toOrigin.y = state->point.y - state->org.y;
 		toOrigin.z = state->point.z - state->org.z;
 
-		//mi_vector_neg(&toOrigin);
-
 		if (debug == 1) {
 			cout << "neg toCenter : " << toCenter.x << " / " << toCenter.y << " / " << toCenter.z << endl;
 		}
@@ -100,18 +87,10 @@ miBoolean observatoryBlackHoleShader (
 		}
 
 		miScalar radianAngle = (2.0f * radius) / (distance);
-		
-		//radianAngle = radianAngle - ((2.0f * radius) / radius);
-		//radianAngle = toRadian(radianAngle);
-
-		//radianAngle = M_PI - radianAngle;
-
-		//miScalar radianAngle = toRadian((M_PI / 2.0) - degree);
-
-		//cout << "degree / radianAngle : " << degree << " / " << radianAngle << endl;
-		if (debug == 1) {
-			cout << "radianAngle : " << radianAngle << endl;
-		}
+		radianAngle = radianAngle - ((2.0f * radius) / radius);
+		radianAngle = toRadian(radianAngle);
+		radianAngle = M_PI - radianAngle;
+		miScalar radianAngle = toRadian((M_PI / 2.0) - degree);
 
 		miVector newDir;
 		miMatrix rotateMat;
@@ -119,9 +98,9 @@ miBoolean observatoryBlackHoleShader (
 		mi_vector_transform(&newDir, &toOrigin, rotateMat);
 		mi_vector_normalize(&newDir);
 		
-//		newDir.x = 1.0f;
-		//newDir.y = 0;
-		//newDir.z = 0;
+		newDir.x = 1.0f;
+		newDir.y = 0;
+		newDir.z = 0;
 
 		state->point = middle;
 		state->dir = newDir;
@@ -130,51 +109,33 @@ miBoolean observatoryBlackHoleShader (
 			cout << "org / dir : " << state->org.x << " / " << state->org.y << " / " << state->org.z << " | " << newDir.x << " / " << newDir.y << " / " << newDir.z << endl;
 		}
 
-		/*
-		newDir.x = (newDir.x + 1) / 2.0f;
-		newDir.y = (newDir.y + 1) / 2.0f;
-		newDir.z = (newDir.z + 1) / 2.0f;
-
-		result->r = newDir.x;
-		result->g = newDir.y;
-		result->b = newDir.z;
-
-		return miTRUE;
-		*/
-
-		//mi_trace_transparent (result, state);
 		mi_trace_refraction(result, state, &newDir);
 
 		if (debug == 1) {
 			cout << "result : " << result->r << " / " << result->g << " / " << result->b << endl;
 		}
-	} else {
-		if (debug == 1) {
-			cout << "========================================================" << endl;
-		}
+    } else {
 		miVector current = state->point;
 		miVector origin = state->org;
 
-		if (debug == 1) {
-			cout << "current / origin : " << current.x << " / " << current.y << " / " << current.z << " | " << origin.x << " / " << origin.y << " / " << origin.z << endl;
-		}
-
 		mi_trace_transparent (result, state);
 	}
-	//miColor opacity;
-	/*
+
+	miColor opacity;
 	opacity.r = 1.0 - transparency;
 	opacity.g = 1.0 - transparency;
 	opacity.b = 1.0 - transparency;
 	opacity.a = 1.0 - transparency;
-	*/
-	//mi_opacity_set(state, &opacity);
-	//result->r = result->r * opacity.r;
-	//result->g = result->g * opacity.g;
-	//result->b = result->b * opacity.b;
-	//result->r = 1.0;
-	//result->g = 0;
-	//result->b = 0;
+
+	mi_opacity_set(state, &opacity);
+
+	result->r = result->r * opacity.r;
+	result->g = result->g * opacity.g;
+	result->b = result->b * opacity.b;
+	result->r = 1.0;
+	result->g = 0;
+	result->b = 0;
+
 	return miTRUE;
 }
 
